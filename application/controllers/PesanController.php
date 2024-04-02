b067e6bbbe33b8e8a2676870a36c58acb862c532
Zul80 <96922388+Zul80@users.noreply.github.com>
2024-04-02 13:11:58 +0700

    comit t

--- application/controllers/PesanController.php
+++ application/controllers/PesanController.php
@@ -714,6 +714,183 @@ class PesanController extends CI_Controller
 		$this->load->view('frontend/pesanan/a5brosur');
 		$this->load->view('frontend/templates/footer');
 	}
+	public function pesanA6brosur()
+	{
+
+		if (isset ($_POST['keranjang'])) {
+			$a6brosurId = 'A5B-' . substr(time(), 5);
+			$bahan = $this->input->post('bahan');
+			$sisi = $this->input->post('sisi');
+			$laminasi = $this->input->post('laminasi');
+			$lipat = $this->input->post('lipat');
+			$jumlah = $this->input->post('jumlah');
+			$estimasi = $this->input->post('estimasi');
+			$total = 0;
+			
+			if ($bahan === "HVS 100") {
+				if ($sisi === "1 Muka" && $lipat === "Tanpa Lipat") {
+					$total = $jumlah * 115000;
+				} else if ($sisi === "1 Muka" && $lipat === "Lipat") {
+					$total = $jumlah * 135000;
+				}
+				if ($sisi === "2 Muka" && $lipat === "Tanpa Lipat") {
+					$total = $jumlah * 230000;
+				} else if ($sisi === "2 Muka" && $lipat === "Lipat") {
+					$total = $jumlah * 250000;
+				}
+			}
+
+			if ($bahan === "Art Paper 120"){
+				if ($sisi === "1 Muka" && $laminasi === "Tanpa Laminasi" && $lipat === "Tanpa Lipat") {
+					$total = $jumlah * 200000;
+				} else if ($sisi === "1 Muka" && $laminasi === "Tanpa Laminasi" && $lipat === "Lipat") {
+					$total = $jumlah * 225000;
+			}
+				if ($sisi === "2 Muka" && $laminasi === "Tanpa Laminasi" && $lipat === "Tanpa Lipat") {
+					$total = $jumlah * 400000;
+				} else if ($sisi === "2 Muka" && $laminasi === "Tanpa Laminasi" && $lipat === "Lipat") {
+					$total = $jumlah * 425000;
+			}
+		}
+			if ($bahan === "Art Paper 150"){
+				if ($sisi === "1 Muka" && $laminasi === "Tanpa Laminasi" && $lipat === "Tanpa Lipat") {
+					$total = $jumlah * 130000;
+				} else if ($sisi === "1 Muka" && $laminasi === "Tanpa Laminasi" && $lipat === "Lipat") {
+					$total = $jumlah * 150000;
+			}
+				if ($sisi === "2 Muka" && $laminasi === "Tanpa Laminasi" && $lipat === "Tanpa Lipat") {
+					$total = $jumlah * 260000;
+				} else if ($sisi === "2 Muka" && $laminasi === "Tanpa Laminasi" && $lipat === "Lipat") {
+					$total = $jumlah * 280000;
+			}
+		}
+		
+			if ($bahan === "Art Carton 210") {
+				if (
+					$sisi === "1 Muka" &&
+					$laminasi === "Tanpa Laminasi" &&
+					$lipat === "Tanpa Lipat"
+				) {
+					$total = $jumlah * 280000;
+				} else if (
+					$sisi === "1 Muka" &&
+					$laminasi === "Tanpa Laminasi" &&
+					$lipat === "Lipat"
+				) {
+					$total = $jumlah * 330000;
+				} else if (
+					$sisi === "1 Muka" &&
+					$laminasi !== "Tanpa Laminasi" &&
+					$lipat === "Tanpa Lipat"
+				) {
+					$total = $jumlah * 405000;
+				} else if (
+					$sisi === "1 Muka" &&
+					$laminasi !== "Tanpa Laminasi" &&
+					$lipat === "Lipat"
+				) {
+					$total = $jumlah * 455000;
+				}
+				if (
+					$sisi === "2 Muka" &&
+					$laminasi === "Tanpa Laminasi" &&
+					$lipat === "Tanpa Lipat"
+				) {
+					$total = $jumlah * 560000;
+				} else if (
+					$sisi === "2 Muka" &&
+					$laminasi === "Tanpa Laminasi" &&
+					$lipat === "Lipat"
+				) {
+					$total = $jumlah * 610000;
+				} else if (
+					$sisi === "2 Muka" &&
+					$laminasi !== "Tanpa Laminasi" &&
+					$lipat === "Tanpa Lipat"
+				) {
+					$total = $jumlah * 810000;
+				} else if (
+					$sisi === "2 Muka" &&
+					$laminasi !== "Tanpa Laminasi" &&
+					$lipat === "Lipat"
+				) {
+					$total = $jumlah * 860000;
+				}
+			}
+		
+		
+
+
+
+			$config['upload_path'] = './assets/images/a6brosur/';
+			$config['allowed_types'] = 'jpg|png|jpeg|zip|rar|pdf';
+			$this->load->library('upload', $config);
+			$this->upload->initialize($config);
+
+			if (!$this->upload->do_upload('upload')) {
+				$error = array('error' => $this->upload->display_errors());
+				var_dump($error);
+			} else {
+				$foto = $this->upload->data('file_name');
+
+				$dataA6brosur = array(
+					'a6brosur_id' => $a6brosurId,
+					'a6brosur_bahan' => $bahan,
+					'a6brosur_jumlah' => $jumlah,
+					'a6brosur_estimasi' => $estimasi,
+					'a6brosur_total' => $total,
+					'a6brosur_foto' => $foto,
+				);
+
+				$allCart = $this->BayarModel->lihat_keranjang();
+				$undoneCart = $this->BayarModel->lihat_keranjang_status($this->session->userdata('session_id'), 'belum')->row_array();
+
+				if ($allCart == null) {
+					$cartId = 'A5B-' . substr(time(), 5);
+					$dataA6brosur['a6brosur_keranjang_id'] = $cartId;
+					$dataCart = array(
+						'keranjang_id' => $cartId,
+						'keranjang_pengguna_id' => $this->session->userdata('session_id'),
+						'keranjang_total' => $total,
+					);
+					$this->PesanModel->simpan_a6brosur($dataA6brosur);
+					$this->BayarModel->simpan_keranjang($dataCart);
+					$this->session->set_flashdata('alert', 'pesan_sukses');
+					redirect('a6brosur');
+				} else {
+					if ($undoneCart != null) {
+						$cartId = $undoneCart['keranjang_id'];
+						$cartTotal = $undoneCart['keranjang_total'];
+						$dataA6brosur['a6brosur_keranjang_id'] = $cartId;
+						$dataCart['keranjang_total'] = $cartTotal + $total;
+
+						$this->PesanModel->simpan_a6brosur($dataA6brosur);
+						$this->BayarModel->update_keranjang($cartId, $dataCart);
+						$this->session->set_flashdata('alert', 'pesan_sukses');
+						redirect('a6brosur');
+					} else {
+						$cartId = 'A5B-' . substr(time(), 5);
+						$dataA6brosur['a6brosur_keranjang_id'] = $cartId;
+						$dataCart = array(
+							'keranjang_id' => $cartId,
+							'keranjang_pengguna_id' => $this->session->userdata('session_id'),
+							'keranjang_total' => $total,
+						);
+						$this->PesanModel->simpan_a6brosur($dataA6brosur);
+						$this->BayarModel->simpan_keranjang($dataCart);
+						$this->session->set_flashdata('alert', 'pesan_sukses');
+						redirect('a6brosur');
+					}
+				}
+			}
+		}
+		$data = array(
+			'title' => 'Pesan Brosur A5 | Nakula Sadewa Digital'
+		);
+		$this->load->view('frontend/templates/header', $data);
+		$this->load->view('frontend/pesanan/a6brosur');
+		$this->load->view('frontend/templates/footer');
+	}
 
 	public function pesanKalender()
 	{
