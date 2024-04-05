319e5b068b16534becf17af6e2a4a2c8b343fb04
Zul80 <zulian.nz80@gmail.com>
2024-04-05 23:28:20 +0700

    updat index

--- application/public/index.php
+++ application/public/index.php
@@ -31,7 +31,7 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
 
 // Load our paths config file
 // This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '..sipesan/app/Config/Paths.php';
+require FCPATH . '..sipesan/application/Config/Paths.php';
 // ^^^ Change this line if you move your application folder
 
 $paths = new Config\Paths();
